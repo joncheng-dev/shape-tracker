f4a05fbc41f31194ad5492b19670aadfe76cf65c
Jonathan Cheng <jcheng461@gmail.com>
2022-03-01 23:48:43 +0900

    output to html page text which tells the user if the sides they entered for a triangle. If so, then it specifies which type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-// Business logic -- functions to calculate what type of triangle it is
+// Business logic -- function to decide if sides form a triangle
+// If it does, determine what type of triangle it is
 function triangle(side1, side2, side3) {
   if (side1 + side2 > side3) {
     if (side1 + side3 > side2) {
@@ -27,17 +28,16 @@ function triangle(side1, side2, side3) {
           if (side1 != side3) {
             if (side2 != side3) {
               // Scalene triangle
-              return 4;
+              return 3;
             }
           }
         }
       }
     }
   } else {
+    // NOT a Triangle
     return 5;
   }
-
-  // NOT a Triangle
 }
 
 // User interface logic -- user enters information & send it to functions
@@ -52,6 +52,14 @@ $(document).ready(function () {
 
     const result = triangle(s1, s2, s3);
     // Send result to html page.
-    $("#output").text(result);
+    if (result === 1) {
+      $("#output").text("These sides form an Equilateral Triangle.");
+    } else if (result === 2) {
+      $("#output").text("These sides form an Isosceles Triangle.");
+    } else if (result === 3) {
+      $("#output").text("These sides form a Scalene Triangle.");
+    } else {
+      $("#output").text("These sides do not form a triangle.");
+    }
   });
 });
